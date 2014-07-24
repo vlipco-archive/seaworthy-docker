67338903839dd184b39f52a8faeb9e26ccbe35e3
David Pelaez <david@vlipco.co>
2014-07-23 20:14:21 -0500

    improved router with domain handler

--- srv-router/conf/srv_router.lua
+++ srv-router/conf/srv_router.lua
@@ -10,26 +10,52 @@ function log(msg)
     ngx.log(ngx.ERR, msg, "\n")
 end
 
--- ngx.say("Checking if it's in the " .. #domains .." known domains")
+-- log("Checking if it's in the " .. #domains .." known domains")
+
+-- for k,domain in pairs(domains) do
+--         log(" - " .. domain)
+-- end
+
+function service_name(reqdomain)
+        for k,domain in pairs(domains) do
+                --log("matching " .. reqdomain .. " with " .. domain)
+                if string.match(reqdomain, domain) then
+                        --log("matched!")
+                        pattern = "%.?" .. domain -- includes separation dot if present
+                        --log("pattern: " .. pattern)
+                        upstream = string.gsub(reqdomain,pattern,"")
+                        if upstream == "" then
+                                return "home"
+                        end
+                        -- return leftmost zone (in case further subdomains exists)
+                        return string.match(upstream,"[^\\.]*$")
+                else
+                        log("no match :(")
+                end
+        end
+        return "unknown"
+end
+
+--log("Service should be in " .. service_name(ngx.var.http_host))
 -- TODO remove domain from host
 -- TODO handle root of the domain
 
-local query_subdomain = ngx.var.http_host .. "." .. ngx.var.target_domain
+local query_subdomain = service_name(ngx.var.http_host) .. "." .. ngx.var.target_domain
 local nameserver = {ngx.var.ns_ip, ngx.var.ns_port}
 
-local dns, err = resolver:new{ 
+local dns, err = resolver:new{
   nameservers = {nameserver}, retrans = 2, timeout = 250
 }
 
 if not dns then
-	log("failed to instantiate the resolver: " .. err)
+        log("failed to instantiate the resolver: " .. err)
     return abort("DNS error", 500)
 end
-
+log("Querying " .. query_subdomain)
 local records, err = dns:query(query_subdomain, {qtype = dns.TYPE_SRV})
 
 if not records then
-	log("failed to query the DNS server: " .. err)
+        log("failed to query the DNS server: " .. err)
     return abort("Internal routing error", 500)
 end
 
@@ -47,8 +73,8 @@ if records[1].port then
     -- resolve the target to an IP
     local target_ip = dns:query(records[1].target)[1].address
     -- pass the target ip to avoid resolver errors
-	ngx.var.target = target_ip .. ":" .. records[1].port
+        ngx.var.target = target_ip .. ":" .. records[1].port
 else
-	log("DNS answer didn't include a port")
-	return abort("Unknown destination port", 500)
+        log("DNS answer didn't include a port")
+        return abort("Unknown destination port", 500)
 end
